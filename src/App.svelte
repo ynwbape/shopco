dccf8596ba6b50e22ba32be40f7940fd9175133a
Pierre nangi <71530777+ynwbape@users.noreply.github.com>
2023-10-28 12:35:56 +0200

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import SignUpOffer from "./lib/components/SignUpOffer.svelte";
+  import Navbar from "./lib/components/Navbar.svelte";
+  import "./styles/colors.css"
+  import Partners from "./lib/components/Partners.svelte";
+</script>
+
+<SignUpOffer />
+<Navbar />
+
+<main>
+  <Partners />
+</main>
+
+<style>
+  
+</style>
---
+++ src/lib/components/Partners.svelte
@@ -0,0 +1,42 @@
+<script>
+    import zara from "../../assets/img/brands/zara.png"
+    import versace from "../../assets/img/brands/versace.png"
+    import prada from "../../assets/img/brands/prada.png"
+    import gucci from "../../assets/img/brands/gucci.png"
+    import cklein from "../../assets/img/brands/calvinklein.png"
+
+    const brands = [zara, versace, prada, gucci, cklein]
+</script>
+
+<div class="partners">
+    {#each brands as brand }
+        <div class="brand">
+            <img src={brand} alt="Marques Partenaires">
+        </div>
+    {/each}
+</div>
+
+<style>
+    .partners {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 141px;
+        height: 162px;
+        background-color: var(--black);
+        animation: infinite linear 2s reverse both;
+    }
+
+    .brand img {
+        height: 44px;
+    }
+
+    @keyframes loop {
+        from {
+            transform: rotateY(0deg);
+        }  
+        to {
+            transform: rotateY(360deg);
+        }
+    }
+</style>
